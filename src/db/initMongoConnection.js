a2c67e28d303427f90a83bf0e8c6619f06977844
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-07-20 12:46:25 +0300

    main

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 
 async function initMongoConnection() {
   try {
-    const MONGODB_URL = process.env.MONGODB_URL;
-
-    await mongoose.connect(MONGODB_URL);
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
+    const password = process.env.MONGODB_PASSWORD;
+    const user = process.env.MONGODB_USER;
+    await mongoose.connect(`mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`);
 
     console.log('Database connection successfully');
   } catch (error) {
