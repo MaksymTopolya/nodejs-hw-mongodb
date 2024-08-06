e4d45213c00636b1b3a4e89c40267e6fc79b16c8
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-08-06 19:16:18 +0300

    main

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ import contactRouter from './routers/contacts.js';
 import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-
+import authRouter from './routers/auth.js';
 
 
 const app = express();
 app.use(cookieParser());
+app.use(authRouter);
 app.use(contactRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
